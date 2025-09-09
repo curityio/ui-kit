b46ba47faa4ee3a941544587cbf9108e673fc48f
Urban Sanden <hej@urre.me>
2025-09-09 11:03:18 +0200

    Add Vite config

---
+++ src/identity-server/styles/index.js
@@ -0,0 +1,5 @@
+// Note: We will import all CSS files in this entry point, then the CSS bundle is produced by Vite
+// Note: Urban will move from .scss to vanilla .css
+
+import "./another-file.css";
+import "./test.css";
