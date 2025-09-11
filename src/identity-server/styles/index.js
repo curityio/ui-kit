20665b81370f4dcb2c7d432e590bc488b1774db7
Urban Sanden <hej@urre.me>
2025-09-11 18:03:55 +0200

    Progress work on CSS

--- src/identity-server/styles/index.js
+++ src/identity-server/styles/index.js
@@ -1,5 +1,29 @@
-// Note: We will import all CSS files in this entry point, then the CSS bundle is produced by Vite
-// Note: Urban will move from .scss to vanilla .css
+/* Curity CSS */
+import "@curity-ui-kit/css/lib/dist/index.css";
 
-import "./another-file.css";
-import "./test.css";
+/* Application styles */
+import "./account-linking.css";
+import "./consent.css";
+import "./deviceflow.css";
+import "./dialog.css";
+import "./done-checkmark.css";
+import "./input-requirements.css";
+import "./ionicons.css";
+import "./login-base.css";
+import "./login-chooser.css";
+import "./login-header.css";
+import "./login-iframe.css";
+import "./login-powered-by.css";
+import "./login-skin.css";
+import "./login-symbol.css";
+import "./login-waiting.css";
+import "./login-well.css";
+import "./opt-in-mfa.css";
+import "./recipient.css";
+import "./spinner.css";
+import "./well.css";
+
+/* TODO: Check what styles from src/identity-server/styles/scss/utilities is needed. Probably none */
+
+/* Curity Theme */
+import "./curity-theme.css";
