012a21d3c638e7529c3445b092e8139c6729e578
Urban Sanden <hej@urre.me>
2025-11-05 14:27:16 +0100

    Minor CSS Library Docs changes

--- src/common/css/docs/src/components/Intro.tsx
+++ src/common/css/docs/src/components/Intro.tsx
@@ -2,7 +2,7 @@ export const Intro: React.FC = ({ children }) => (
   <div className="py2">
     <div className="py2 mw-40 mx-auto">
       <p>Curity CSS Toolkit</p>
-      <h1 className="type-extra-bold">The baseline CSS toolkit for that Curity look and feel.</h1>
+      <h1>The baseline CSS toolkit for that Curity look and feel.</h1>
       <p>
         A minimalistic approach to styling Curity projects that live in a browser. Fast,
         easy-to-understand and without side effects.
