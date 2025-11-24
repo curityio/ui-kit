6c5668c43b642dddf7415b77e0672132fe10dea3
Urban Sanden <hej@urre.me>
2025-11-24 09:13:02 +0100

    IS-10358 Cleanup icons package, cleanup readme, remove example folder, simplify build

--- src/common/icons/rollup.config.js
+++ src/common/icons/rollup.config.js
@@ -2,16 +2,13 @@ import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import { defineConfig } from "rollup";
-import dts from "rollup-plugin-dts";
 import { terser } from "rollup-plugin-terser";
-
-const isSSP = process.env.BUILD_VARIANT === "ssp";
 
 export default defineConfig([
   {
-    input: isSSP ? "src-ssp/index.ts" : "src/index.ts",
+    input: "src/index.ts",
     output: {
-      file: isSSP ? "dist-ssp/index.js" : "dist/index.js",
+      file: "dist/index.js",
       format: "esm",
       sourcemap: true,
     },
@@ -19,7 +16,7 @@ export default defineConfig([
       resolve(),
       commonjs(),
       typescript({
-        tsconfig: isSSP ? "./tsconfig.ssp.json" : "./tsconfig.json",
+        tsconfig: "./tsconfig.json",
         sourceMap: true,
         inlineSources: true,
       }),
