b706878637ce4cef77626eaa4a2789e84ccf575a
Urban Sanden <hej@urre.me>
2025-12-16 15:38:59 +0100

    IS-10687 Export UserMenu from Component Library

--- src/common/component-library/src/components/index.ts
+++ src/common/component-library/src/components/index.ts
@@ -4,3 +4,4 @@ export { Button } from './Button';
 export { List } from './list/list/List';
 export { ListCell } from './list/list-cell/ListCell';
 export { ListRow } from './list/list-row/ListRow';
+export { UserMenu } from './user-menu/UserMenu';
