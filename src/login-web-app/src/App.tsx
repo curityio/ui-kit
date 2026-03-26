4f9330066ea1a8db06433e754f5bf6c7fd4b151a
Aleix Suau <info@macrofonoestudio.es>
2026-03-26 14:43:50 +0100

    IS-11176: fix formatting

--- src/login-web-app/src/App.tsx
+++ src/login-web-app/src/App.tsx
@@ -14,16 +14,15 @@ import { DevBar } from './shared/ui/devbar/DevBar';
 import { ErrorBoundary } from './shared/feature/error-handling/ErrorBoundary';
 import { HaapiStepperStepUI } from './haapi-stepper/feature/steps/HaapiStepperStepUI';
 import { HaapiStepper } from './haapi-stepper/feature/stepper/HaapiStepper';
-import { HaapiStepperErrorNotifier } from './haapi-stepper/feature';
 
 export function App() {
   return (
     <ErrorBoundary>
       <HaapiStepper>
-          <Layout>
-            <DevBar />
-            <HaapiStepperStepUI />
-          </Layout>
+        <Layout>
+          <DevBar />
+          <HaapiStepperStepUI />
+        </Layout>
       </HaapiStepper>
     </ErrorBoundary>
   );
