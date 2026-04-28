6d19be4889cc56c9fd585bc2f5bd463fcc8ea050
Urban Sanden <hej@urre.me>
2026-04-28 12:05:04 +0200

    IS-11244 Apply color prop to Apple and Entrust authenticator icons.

--- src/common/icons/src/components/authenticator/IconAuthenticatorEntrust.tsx
+++ src/common/icons/src/components/authenticator/IconAuthenticatorEntrust.tsx
@@ -15,7 +15,7 @@ const SvgIconAuthenticatorEntrust = (props: SVGProps<SVGSVGElement>) => (
       d="m128 207-68.42-39.5v-79L128 49l68.42 39.5v79.01L128 207.01Zm57.41-45.85v-66.3L127.99 61.7 70.57 94.85v66.3l57.42 33.15z"
     />
     <path
-      fill="url(#Namnl\xF6s_\xF6vertoning_2)"
+      fill="#762b8a"
       d="M138.83 164.51v-11.08l35.54-20.67v-11l-35.54 20.47v-11.01l35.58-20.46v-9.55L128 74.41l-46.41 26.8v53.59L128 181.59l46.41-26.81v-11z"
     />
   </svg>
